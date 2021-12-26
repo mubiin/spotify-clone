2c7cb7475083f7ef7a31bd4a00f789f3c68b542c
Mubin Salehin <mubin.salehin@gmail.com>
2021-12-27 01:18:03 +0600

    add dummy playlists to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,8 @@ import {
   LibraryIcon,
   PlusCircleIcon,
   HeartIcon,
-  RssIcon
+  RssIcon,
+  MusicNoteIcon
 } from "@heroicons/react/outline";
 
 function Sidebar() {
@@ -12,6 +13,7 @@ function Sidebar() {
   const iconStyle = "h-5 w-5";
   return (
     <div className="text-gray-500 p-5 space-y-4 text-sm border-r-gray-900">
+      {/* Main buttons */}
       <button className={buttonStyle}>
         <HomeIcon className={iconStyle} />
         <p>Home</p>
@@ -24,7 +26,9 @@ function Sidebar() {
         <LibraryIcon className={iconStyle} />
         <p>Your Library</p>
       </button>
+
       <hr className="border-t-[0.1px] border-gray-900" />
+
       <button className={buttonStyle}>
         <PlusCircleIcon className={iconStyle} />
         <p>Create Playlist</p>
@@ -36,6 +40,22 @@ function Sidebar() {
       <button className={buttonStyle}>
         <RssIcon className={iconStyle} />
         <p>Your Episodes</p>
+      </button>
+
+      <hr className="border-t-[0.1px] border-gray-900" />
+
+      {/* Playlists */}
+      <button className={buttonStyle}>
+        <MusicNoteIcon className={iconStyle} />
+        <p>Playlist 1</p>
+      </button>
+      <button className={buttonStyle}>
+        <MusicNoteIcon className={iconStyle} />
+        <p>Playlist 2</p>
+      </button>
+      <button className={buttonStyle}>
+        <MusicNoteIcon className={iconStyle} />
+        <p>Playlist 3</p>
       </button>
     </div>
   );
