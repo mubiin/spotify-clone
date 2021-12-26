bc87723e86da7314af5a6e6ddfda333ef9e4ae8a
Mubin Salehin <mubin.salehin@gmail.com>
2021-12-26 23:27:15 +0600

    update tailwind config to purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
